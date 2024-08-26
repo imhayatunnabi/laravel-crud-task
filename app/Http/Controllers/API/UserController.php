ccfa0208b0d3ef68b8a9d27c161d6ec09ad19648
imhayatunnabi <imhayatunnabi@gmail.com>
2024-08-26 22:41:11 +0600

    user controller user index method api has been developed

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $perPage = request()->input('perPage', 20);
+        $search = request()->input('search');
+        $users = User::where('name', 'like', '%' . $search . '%')
+            ->orWhere('email', 'like', '%' . $search . '%')
+            ->orWhere('slug', 'like', '%' . $search . '%')->paginate($perPage);
+        return $this->responseWithSuccess('User List', $users);
+    }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function responseWithSuccess($message, $data, $status = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'status' => $status,
+            'data' => $data,
+        ], $status);
+    }
+
+    public function responseWithError($message, $data, $status = 500)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'status' => $status,
+            'data' => $data,
+        ], $status);
+    }
 }

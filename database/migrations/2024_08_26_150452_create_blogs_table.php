daa0c75589069ad404c2d8a054b87d5969b9723c
imhayatunnabi <imhayatunnabi@gmail.com>
2024-08-26 21:09:08 +0600

    laravel crud operation model migration implementation

---
+++ database/migrations/2024_08_26_150452_create_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->string('slug', 255)->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};

0bc1783752fe21d05ddc11cc905e9ca0bf617591
imhayatunnabi <imhayatunnabi@gmail.com>
2024-08-29 12:53:02 +0600

    user crud done

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
 
 class UserController extends Controller
 {
@@ -15,6 +21,67 @@ class UserController extends Controller
         $users = User::where('name', 'like', '%' . $search . '%')
             ->orWhere('email', 'like', '%' . $search . '%')
             ->orWhere('slug', 'like', '%' . $search . '%')->paginate($perPage);
-        return $this->responseWithSuccess('User List', $users);
+        return $this->responseWithSuccess('User List', $users, Response::HTTP_OK);
+    }
+    public function store(UserStoreRequest $userStoreRequest)
+    {
+        DB::beginTransaction();
+
+        try {
+            $user = User::create($userStoreRequest->validated());
+            DB::commit();
+            return $this->responseWithSuccess('User Created Successfully', $user, Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error('User creation failed: ', ['exception' => $th]);
+            return $this->responseWithError('User Creation Failed', [], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function edit($slug)
+    {
+        $user = User::findBySlug($slug);
+        if ($user) {
+            return $this->responseWithSuccess('User Found', $user, Response::HTTP_OK);
+        } else {
+            return $this->responseWithError('User Not Found', '', Response::HTTP_NOT_FOUND);
+        }
+    }
+    public function update(UserUpdateRequest $userUpdateRequest, $slug)
+    {
+        $user = User::findBySlug($slug);
+        if (!$user) {
+            return $this->responseWithError('User Not Found', [], Response::HTTP_NOT_FOUND);
+        }
+        try {
+            DB::beginTransaction();
+            if (!$user) {
+                return $this->responseWithError('User Not Found', [], Response::HTTP_NOT_FOUND);
+            }
+            $user->update($userUpdateRequest->valslugated());
+            DB::commit();
+            return $this->responseWithSuccess('User Updated Successfully', $user, Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error('User update failed: ', ['exception' => $th]);
+            return $this->responseWithError('User Update Failed', [], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function destroy($slug){
+        $user = User::findBySlug($slug);
+        if (!$user) {
+            return $this->responseWithError('User Not Found', [], Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            DB::beginTransaction();
+            $user->delete();
+            DB::commit();
+            return $this->responseWithSuccess('User Deleted Successfully', [], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error('User deletion failed: ', ['exception' => $th]);
+            return $this->responseWithError('User Deletion Failed', [], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'user'], function() {
+    Route::get('index',[UserController::class, 'index'])->name('index');
+    Route::get('store',[UserController::class, 'store'])->name('store');
+    Route::get('edit/{slug}',[UserController::class, 'edit'])->name('edit');
+    Route::get('update/{slug}',[UserController::class, 'update'])->name('update');
+    Route::get('delete/{slug}',[UserController::class, 'delete'])->name('delete');
 });
+Route::group(['prefix' => 'blog'], function() {
+    Route::get('index',[UserController::class, 'index'])->name('index');
+    Route::get('store',[UserController::class, 'store'])->name('store');
+    Route::get('edit/{slug}',[UserController::class, 'edit'])->name('edit');
+    Route::get('update/{slug}',[UserController::class, 'update'])->name('update');
+    Route::get('delete/{slug}',[UserController::class, 'delete'])->name('delete');
+});
+
